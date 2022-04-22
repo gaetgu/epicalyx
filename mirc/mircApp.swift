66d58e55600ad1578d77cab41d67337843ca1aa5
gaetgu <gabriel.ethan@gmail.com>
2022-04-22 10:40:31 -0600

    Initial Commit

---
+++ mirc/mircApp.swift
@@ -0,0 +1,17 @@
+//
+//  mircApp.swift
+//  mirc
+//
+//  Created by Gabriel Gutierrez on 4/22/22.
+//
+
+import SwiftUI
+
+@main
+struct mircApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
